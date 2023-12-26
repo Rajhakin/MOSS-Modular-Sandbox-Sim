a26ca672a39f9ff5bf54d1d91898148278b1ab1e
Rajhakin <145900001+Rajhakin@users.noreply.github.com>
2023-12-26 12:26:47 -0300

    feat: add debug overlay to display world obj data

---
+++ scenes/ui_canvas/debug_overlay.gd
@@ -0,0 +1,23 @@
+extends PanelContainer
+
+
+var agent_data : AgentData
+
+@onready var label: Label = $MarginContainer/Label
+
+
+func _ready() -> void:
+	Events.agent_mouse_entered.connect(_on_agent_mouse_entered)
+	
+
+func _physics_process(delta: float) -> void:
+	if agent_data:
+		var text = "name: %s \nstatus: %s \nposition: %.v \nrotation: %s \nspeed: %s" % [
+					agent_data.name, agent_data.current_status, agent_data.position, agent_data.rotation, "null"]
+		label.text = text
+	
+
+func _on_agent_mouse_entered(data):
+	agent_data = data
+	
+	
